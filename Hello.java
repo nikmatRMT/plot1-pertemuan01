7729ae6dd8c9376ab73017e854029f468ec6cd99
nikmatRMT <nikforart@gmail.com>
2024-03-25 23:29:49 +0800

    Hello git  & java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
- public static void main (string [] args){
-  system.out.println("hello world!!");
+ public static void main (String [] args){
+  System.out.println("hello world!!");
+  System.out.println("hello Java!!");
  }
 }
